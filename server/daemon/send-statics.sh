8eb3beb5159f6a6423821d0c0361405d783480d5
Jonathan <jonathan.tierno@gmail.com>
2016-12-07 10:43:24 -0300

    cambios varios

--- server/daemon/send-statics.sh
+++ server/daemon/send-statics.sh
@@ -2,7 +2,6 @@
 
 
 INTERVAL='2s'
-BROKER_URL='localhost'
 BROKER_PORT=1883
 TOPIC="computadoras/$1/carga"
 while [ true ]
@@ -10,5 +9,5 @@ do
     sleep $INTERVAL
     FREEMEM=$(cat /proc/meminfo | grep MemFree | sed -r 's/[^0-9]*([0-9]+)[^0-9]*/\1/g')
     CPULOAD=$(cat /proc/loadavg | cut -f 1 -d' ')
-    mosquitto_pub  -h $BROKER_URL -p $BROKER_PORT -t $TOPIC -m "$FREEMEM;$CPULOAD"
+    mosquitto_pub  -h $2 -p $BROKER_PORT -t $TOPIC -m "$FREEMEM;$CPULOAD"
 done
